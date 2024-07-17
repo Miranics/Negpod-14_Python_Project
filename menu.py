5eaba6e58f7f1cad9afe532bdc2c90fd2f900084
cinna03 <c.mwiza1@alustudent.com>
2024-07-17 18:06:01 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,8 +2,9 @@
 
 menu = [
     {"name": "Margherita Pizza", "price": 12000},
-    {"name": "Pepperoni Pizza", "price": 12000}
-    {"name": "BBQ Chicken Pizza", "price": 12000}
+    {"name": "Pepperoni Pizza", "price": 12000},
+    {"name": "BBQ Chicken Pizza", "price": 12000},
+    {"name": "Hawaiian Pizza", "price": 12000},
     {"name": "Burger", "price": 6500},
     {"name": "Pasta", "price": 7000},
     {"name": "Salad", "price": 8500},
