911bd306f7c1f14c588e4885bd31111b72b5b508
Sharif2138 <169043842+Sharif2138@users.noreply.github.com>
2024-07-28 11:28:13 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -14,14 +14,14 @@ menu = [
     {'name': "Ice Cream", "price": 4000},
     {'name': "Sandwich", "price": 6000},
     {'name': "Coffee", "price": 2500},
-    {"name": "Latte Coffee", "price": 3500},
-    {"name": "Tea", "price": 2000},
-    {"name": "Drinking water", "price": 1000},
-    {"name": "Fresh Juice", "price": 2000},
-    {"name": "Soup", "price": 5000},
-    {"name": "Chicken Wings", "price": 9000},
-    {"name": "Fish and Chips", "price": 10000},
-    {"name": "Smoothie", "price": 3500}
+    {'name': "Latte Coffee", "price": 3500},
+    {'name': "Tea", "price": 2000},
+    {'name': "Drinking water", "price": 1000},
+    {'name': "Fresh Juice", "price": 2000},
+    {'name': "Soup", "price": 5000},
+    {'name': "Chicken Wings", "price": 9000},
+    {'name': "Fish and Chips", "price": 10000},
+    {'name': "Smoothie", "price": 3500}
 ]
 
 def display_menu():
