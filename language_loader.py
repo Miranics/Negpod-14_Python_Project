9586fcd1750a245c6c7a98f61467a810a3bcf5ef
miranics <002nasya@gmail.com>
2024-07-29 11:57:00 +0200

    Feedback and UI Restructure

--- language_loader.py
+++ language_loader.py
@@ -2,10 +2,6 @@
 import json
 
 def load_language(language):
-    try:
-        with open(f'languages/{language}.json', 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print(f"Language file for '{language}' not found. Defaulting to English.")
-        with open('languages/english.json', 'r') as file:
-            return json.load(file)
+    with open(f'languages/{language}.json', 'r') as file:
+        lang = json.load(file)
+    return lang
--- main.py
+++ main.py
@@ -2,13 +2,34 @@
 from order import place_order, review_order, update_order, remove_order, save_order_to_db, give_feedback
 from language_loader import load_language
 
+
 def main():
-    lang = load_language()
+    print("\n-----------------------------")
+    print("Welcome to N14 Restaurant")
+    print("-----------------------------")
+
+    # Prompt user to select a language
+    print("Select Language / Sélectionnez la langue / Hitamo Ururimi:")
+    print("1. English")
+    print("2. French")
+    print("3. Kinyarwanda")
+    lang_choice = input("Enter your choice (1-3): ")
+
+    if lang_choice == '1':
+        lang = load_language('english')
+    elif lang_choice == '2':
+        lang = load_language('french')
+    elif lang_choice == '3':
+        lang = load_language('kinyarwanda')
+    else:
+        print("Invalid choice. Defaulting to English.")
+        lang = load_language('english')
 
     print("\n-----------------------------")
     print(lang['welcome_message_1'])
     print(lang['welcome_message_2'])
     print("-----------------------------")
+
     order = []
     while True:
         print(lang['main_menu'])
@@ -24,7 +45,7 @@ def main():
 
         if choice == '1':
             from menu import display_menu
-            display_menu()
+            display_menu(lang)
         elif choice == '2':
             place_order(order, lang)
         elif choice == '3':
@@ -42,6 +63,7 @@ def main():
             break
         else:
             print(lang['invalid_choice'])
+
 
 if __name__ == "__main__":
     main()
